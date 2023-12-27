b8a38cb6badc05bf55e710bc34abfb5d94ce7f57
kiwon kim <rlarldnjs17@gmail.com>
2023-12-27 09:45:30 +0900

    배민 Clone UI

---
+++ app/src/main/java/com/example/standard/BaeMinMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.standard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaeMinMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
